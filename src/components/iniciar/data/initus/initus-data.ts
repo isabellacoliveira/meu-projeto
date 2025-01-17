99d691c8b106a2d6c387e3f58b00bbd9c2c20028
Isah <isaaholiveira15@gmail.com>
2025-01-16 21:34:35 -0300

    feat: novas entregas

--- src/components/iniciar/data/initus/initus-data.ts
+++ src/components/iniciar/data/initus/initus-data.ts
@@ -1,18 +1,11 @@
 import { CardData } from "../../../../interfaces/card-data";
-import shell from '../../../../assets/shell-gas-stations-front-let.webp'
 import conta from '../../../../assets/cartoes-itau-platinum-laranja-fundo-azul-coracao.avif'
 
 export const initusData: CardData[] = [
     {
-        title: "Adição do modal no fluxo do SHELL V3",
-        description: "Adição do modal nos testes do SHELL V3",
-        imageSrc: shell,
-        entregue: false
-    },
-    {
-        title: "Adição da geração de massas conta-pagamento",
+        title: "Adição da geração de massas conta-pagamento no Gerador de Massas",
         description: "Adição da geração de massas conta-pagamento no portal de massas",
         imageSrc: conta,
-        entregue: false
+        entregue: true
     },
 ];
--- src/routes.tsx
+++ src/routes.tsx
@@ -7,19 +7,19 @@ import Destaques from "./components/destaques";
 
 function AppRoutes() {
     return (
-        <>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Inicio />} >
-                        <Route path="/entregas" element={<Iniciar />} />
-                        <Route path="/destaques" element={<Destaques />} />
-                        <Route path="/skills" element={<Skills />} />
-                        <Route path="/trajetoria" element={<Trajetoria />} />
-                    </Route>
-                    <Route path="/*" element={<Destaques />} />
-                </Routes>
-            </Router>
-        </>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Inicio />} >
+                    <Route index element={<Iniciar />} />
+                    <Route path="/entregas" element={<Iniciar />} />
+                    <Route path="/destaques" element={<Destaques />} />
+                    <Route path="/skills" element={<Skills />} />
+                    <Route path="/trajetoria" element={<Trajetoria />} />
+                </Route>
+                
+                <Route path="/*" element={<Destaques />} />
+            </Routes>
+        </Router>
     );
 }
 
